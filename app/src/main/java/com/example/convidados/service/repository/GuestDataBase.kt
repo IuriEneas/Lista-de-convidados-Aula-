ed1349db7f28caee36e410f137c7d5295c711230
IuriEneas <iurieneas@gmail.com>
2021-09-23 14:50:23 -0300

    Single Commit

---
+++ app/src/main/java/com/example/convidados/service/repository/GuestDataBase.kt
@@ -0,0 +1,33 @@
+package com.example.convidados.service.repository
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.convidados.service.model.GuestModel
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.synchronized
+
+@Database(entities = [GuestModel::class], version = 1)
+abstract class GuestDataBase: RoomDatabase(){
+
+    abstract fun guestDAO(): GuestDao
+
+    companion object{
+
+        private lateinit var INSTANCE: GuestDataBase
+
+
+        @OptIn(InternalCoroutinesApi::class)
+        fun getDatabase(context: Context): GuestDataBase{
+            if(!:: INSTANCE.isInitialized){
+                synchronized(GuestDataBase::class){
+                    INSTANCE = Room.databaseBuilder(context, GuestDataBase::class.java, "GuestDB")
+                        .allowMainThreadQueries()
+                        .build()
+                }
+            }
+            return INSTANCE
+        }
+    }
+}
